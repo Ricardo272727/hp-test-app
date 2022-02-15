f5f3f8401a38a176b52af502f912d284967c5117
Ricardo Segura Cuanalo <cuanaloricardo@gmail.com>
2022-02-14 22:19:22 -0600

    ci: change env.example.js to deploy with process.env.PORT variable

--- env.example.js
+++ env.example.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Harry Potter app',
   lang: 'es',
 	prefix: '/',
-	port: process.env.PORT || 5009,
+	port: 5009,
   api: 'http://localhost:3000',
   localStoragePrefix: 'harry-potter-app-',
   mode: 'development',
